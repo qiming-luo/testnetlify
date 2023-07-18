fa29bb00dde1b89db95f48f45d0be4950045eec8
luoqiming82 <qimingluo28@gmail.com>
2023-07-18 13:57:19 +0800

    build

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const titleStyle = {
+  textAlign: 'center',
+  fontSize: 62,
+  color: 'red'
+}
+
+function Title(){
+  return (
+    <h1 style={titleStyle}>test test test</h1>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Title />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
